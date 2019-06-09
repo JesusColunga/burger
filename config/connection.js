0c6b2d9fcfa14acf2b3bd463fa74b2d6b8bb0c5e
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-06-08 23:21:14 -0500

    JawsDb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // /config/connection.js
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "jc",
-  password: "jcs",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "jc",
+        password: "jcs",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
   if (err) {
